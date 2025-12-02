8f1d5b0dc8607eea406dfd80dadc93596c9a2e71
VickySuraj <vigneshwaranjheyaraman@comcast.com>
2025-12-02 15:47:51 +0530

    modified opencode/tool/rvu fixed bug

--- opencode/tool/rvu.ts
+++ opencode/tool/rvu.ts
@@ -7,15 +7,14 @@ export default tool({
         workingDirectory: tool.schema.string().describe("the working directory which has the service")
     },
     async execute(args, _context) {
-        args.workingDirectory && await Bun.$.cwd(args.workingDirectory).nothrow().quiet();
+        args.workingDirectory && await Bun.$.cwd(args.workingDirectory);
         await Bun.$`git fetch --prune origin`;
         const prNumberString:string|undefined = args.prUrl.match(/\d+/)?.at(0);
         if(prNumberString) {
             let prNumber = parseInt(prNumberString),
                 branchName:string = `pr_${prNumber}`;
-            const gitPRCheckoutCommand:string= `git fetch origin pull/${prNumber}/head:${branchName}`.nothrow().quiet();
-            await Bun.$(gitPRCheckoutCommand).nothrow().quiet();;
-            let result = await Bun.$(`git diff origin/master...${branchName}`).text();
+            await Bun.$`git fetch origin pull/${prNumber}/head:${branchName}`.nothrow().quiet();;
+            let result = await Bun.$`git diff origin/master...${branchName}`.text();
             return result.trim();
         }
     }
