1b2b2e9fab0fa2661145afc45223890c6b0b18f2
VickySuraj <vigneshwaranjheyaraman@comcast.com>
2025-12-02 15:33:49 +0530

    modified opencode/tool/rvu

--- opencode/tool/rvu.ts
+++ opencode/tool/rvu.ts
@@ -4,15 +4,17 @@ export default tool({
     description: "RVU (Re-V-U) i.e., reviewing the changeset given the PR and repository",
     args: {
         prUrl: tool.schema.string().describe("The URL of the pull request to be reviewed"),
+        workingDirectory: tool.schema.string().describe("the working directory which has the service")
     },
     async execute(args, _context) {
-     const gitFetch:string = const prNumberString:string|undefined = args.prUrl.match(/\d+/);
-        await Bun.$`git fetch --prune origin`;      const prNumberString:string|undefined = args.prUrl.match(/\d+/)?.at(0);
+        args.workingDirectory && await Bun.$.cwd(args.workingDirectory).nothrow().quiet();
+        await Bun.$`git fetch --prune origin`;
+        const prNumberString:string|undefined = args.prUrl.match(/\d+/)?.at(0);
         if(prNumberString) {
             let prNumber = parseInt(prNumberString),
                 branchName:string = `pr_${prNumber}`;
-            const gitPRCheckoutCommand:string= `git fetch origin pull/${prNumber}/head:${branchName}`;
-            await Bun.$(gitPRCheckoutCommand);
+            const gitPRCheckoutCommand:string= `git fetch origin pull/${prNumber}/head:${branchName}`.nothrow().quiet();
+            await Bun.$(gitPRCheckoutCommand).nothrow().quiet();;
             let result = await Bun.$(`git diff origin/master...${branchName}`).text();
             return result.trim();
         }
